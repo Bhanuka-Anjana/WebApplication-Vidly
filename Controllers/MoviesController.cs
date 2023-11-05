54cefa87f0d7765febb123e42f5edf4acff61416
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-11-06 04:40:39 +0530

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication_Vidly.Models;
+
+namespace WebApplication_Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Id = 1,
+                Title = "Test",
+                Description = "Test description"
+            };
+
+            return View(movie);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication_Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
